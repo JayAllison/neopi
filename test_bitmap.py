1d001614b99b4b8a480335167061e5a00ce64ffa
Jay Allison <jay.allison@synapsewireless.com>
2018-12-12 18:24:04 -0600

    Can I show two bitmaps on the screen?

--- test_bitmap.py
+++ test_bitmap.py
@@ -4,15 +4,28 @@ from PIL import Image
 import time
 
 PIXEL_COUNT = 256
-BRIGHTNESS = 0.5
+BRIGHTNESS = 0.25
 GPIO = board.D18
 
-pixels = neopixel.NeoPixel(GPIO, PIXEL_COUNT, brightness=BRIGHTNESS, auto_write=True)
+pixels = neopixel.NeoPixel(GPIO, PIXEL_COUNT, brightness=BRIGHTNESS, auto_write=False)
 
-bitmap = Image.open('bitmaps/cross.png')
+bitmaps = [
+    Image.open('bitmaps/cross.png'),
+    Image.open('bitmaps/cross.png')
+]
 
-width, height = bitmap.size
+while True:
 
-for x in range(width):
-    for y in range(height):
-        pixels[x*width ++ y] = bitmap.getpixels((x, y))[0:3]
+    for bitmap in bitmaps:
+
+        width, height = bitmap.size
+
+        for x in range(width):
+            for y in range(height):
+                pixels[x*width + y] = bitmap.getpixel((x, y))[0:3]
+
+        time.sleep(1)
+
+        pixels.fill((0, 0, 0))
+
+        time.sleep(1)
