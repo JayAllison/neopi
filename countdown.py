eff1ed40ba0f1b348a89abbf7529cd3fe5d16d84
Jay Allison <jay.allison@synapsewireless.com>
2018-12-26 17:46:16 -0600

    Adding countdown message

---
+++ countdown.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import argparse
+import board
+import datetime
+import neopixel
+from PIL import ImageFont
+import sprite_writer
+import text_scroller
+import time
+
+X_PIXELS = 16
+Y_PIXELS = 16
+PIXEL_COUNT = X_PIXELS * Y_PIXELS
+BRIGHTNESS = 0.5
+GPIO = board.D18
+
+message = ("Countdown to Walt Disney World:", (255, 255, 255))
+target_date = datetime.datetime(2019, 3, 16, 6, 0, 0)
+date_color = (255, 255, 255)
+image_filename = 'bitmaps/white_mickey_16x16.bmp'
+
+
+def parse_arguments():
+    arg_description = ""
+    arg_epilog = ""
+    parser = argparse.ArgumentParser(description=arg_description, epilog=arg_epilog)
+    parser.add_argument('-b', '--brightness', action='store', dest="brightness", type=float,
+                        default=BRIGHTNESS, help="Brightness level (0.0-1.0)")
+    parser.add_argument('-t', '--timedelay', action='store', dest="timedelay", type=int,
+                        default=BRIGHTNESS, help="Startup time delay")
+    return parser.parse_args()
+
+
+def clear_screen(neopixels):
+    neopixels.fill((0, 0, 0))
+
+
+args = parse_arguments()
+time.sleep(args.timedelay)
+
+pixels = neopixel.NeoPixel(GPIO, PIXEL_COUNT, brightness=args.brightness, auto_write=False)
+scroller = text_scroller.TextScroller(X_PIXELS, Y_PIXELS, ImageFont.truetype("Perfect_DOS_VGA_437_Win.ttf", 16))
+writer = sprite_writer.SpriteWriter(X_PIXELS, Y_PIXELS)
+
+while True:
+
+    writer.fade_in_sprite(pixels, image_filename, 5)
+    time.sleep(2)
+    writer.fade_out_sprite(pixels, image_filename, 5)
+    time.sleep(0.25)
+
+    scroller.scroll_message(pixels, message[0], message[1])
+    clear_screen(pixels)
+    time.sleep(0.25)
+
+    countdown = datetime.datetime.now() - target_date
+    countdown_string = str(countdown.days) + ' days, ' + \
+        str(countdown.seconds/60/60) + ':' + str(countdown.seconds/60) + ':' + str(countdown.seconds)
+    print(countdown_string)
+
+    scroller.scroll_message(pixels, message[0], message[1])
+    clear_screen(pixels)
+    time.sleep(0.25)
